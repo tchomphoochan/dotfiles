3fde00d8658583dbdff52ba1fa290225d7c0af4d
Thanadol Chomphoochan <t.chomphoochan@gmail.com>
2025-03-29 14:17:18 -0400

    Initial lazy.nvim config

---
+++ .config/nvim/lua/config/vim.lua
@@ -0,0 +1,23 @@
+-- Vim options
+
+vim.cmd.colorscheme("tokyonight")
+
+-- line number
+vim.opt.number = true
+ 
+-- tabs
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+ 
+-- visuals
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.termguicolors = true
+ 
+-- search
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
